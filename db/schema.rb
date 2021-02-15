7962edc6feedf2ef990a6d1eacadcbaae091ffea
rathersilly <rathersilly@live.com>
2021-02-15 05:41:58 -0600

    added barebones albums and songs models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_060245) do
+ActiveRecord::Schema.define(version: 2021_02_15_113523) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.datetime "release_date"
+    t.integer "band_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["band_id"], name: "index_albums_on_band_id"
+  end
 
   create_table "bands", force: :cascade do |t|
     t.string "name"
@@ -19,4 +28,14 @@ ActiveRecord::Schema.define(version: 2021_02_15_060245) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "album_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["album_id"], name: "index_songs_on_album_id"
+  end
+
+  add_foreign_key "albums", "bands"
+  add_foreign_key "songs", "albums"
 end
