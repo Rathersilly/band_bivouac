c0e304ace28feb22bee0d2876819f002879dec62
rathersilly <rathersilly@live.com>
2021-02-16 07:10:46 -0600

    added seed scripts and media

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,6 +1,8 @@
 class Band < ApplicationRecord 
   has_many :albums #, dependent: destroy
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true,
+                    length: { maximum: 50 },
+                    uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w\-.\+]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
                       length: { maximum: 255 },
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do |n|
-  name = "#{Faker::Hacker.adjective.capitalize} #{Faker::Creature::Animal.name.capitalize}"
-  email = "#{name.sub(/ /, '_').downcase}#{n}@example.com"
-  Band.create!( name:       name,
-                email:      email)   
+bandname = "Neural Mammoth Motivator"
+band = Band.create!( name: bandname,
+              email: "nmm@example.com")
+band_media_path = Rails.root.join("media",
+                                bandname.gsub(/ /, "_"))
+
+band_media_path.children.each do |album_dir|
+  next unless File.directory?(album_dir)
+  album = band.albums.create!(name: album_dir.basename, release_date: Time.zone.now)
+  band_media_path.join(album_dir).children.each do |song|
+
+    album.songs.create!(name: song.basename) if song.to_s.include?("fake")
+  end
+
+        
 end
+
+
+#10.times do |n|
+#  name = "#{Faker::Hacker.adjective.capitalize} #{Faker::Creature::Animal.name.capitalize}"
+##  email = "#{name.sub(/ /, '_').downcase}#{n}@example.com"
+#  Band.create!( name:       name,
+#                email:      email)   
+#end
+
+
+
+
 
